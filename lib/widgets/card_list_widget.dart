8d06243c213a0b18a7913a3dc997857817c5c7c6
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2023-03-12 00:44:39 -0500

    Se agregaron archivos JSON
    Se crearon archivos JSON con datos de prueba para facilitar la creación de tarjetas.

--- lib/widgets/card_list_widget.dart
+++ lib/widgets/card_list_widget.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 
 class CardListWidget extends StatefulWidget {
@@ -6,7 +7,24 @@ class CardListWidget extends StatefulWidget {
 }
 
 class _CardListWidgetState extends State<CardListWidget> {
-  List<String> _list = ['Actividad 1', 'Actividad 2', 'Actividad 3'];
+  List<RouteData> _list = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    String jsonString =
+        await DefaultAssetBundle.of(context).loadString('assets/routes.json');
+    List<dynamic> jsonList = json.decode(jsonString);
+    List<RouteData> dataList =
+        jsonList.map((json) => RouteData.fromJson(json)).toList();
+    setState(() {
+      _list = dataList;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,21 +33,74 @@ class _CardListWidgetState extends State<CardListWidget> {
       body: ListView.builder(
         itemCount: _list.length,
         itemBuilder: (BuildContext context, int index) {
+          RouteData data = _list[index];
           return Card(
             child: ListTile(
-              title: Text(_list[index]),
+              title: Text(
+                '${data.startDate} ${data.startTime}',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Tiempo: ${data.timeSpent}',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  Text(
+                    'Distancia: ${data.distance} km',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                ],
+              ),
             ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            _list.add('Actividad ${_list.length + 1}');
-          });
+          // not implemented
         },
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class RouteData {
+  final String startDate;
+  final String startTime;
+  final String finishDate;
+  final String finishTime;
+  final double distance;
+  final String timeSpent;
+  final int type;
+  final int routeId;
+  final int userId;
+
+  RouteData({
+    required this.startDate,
+    required this.startTime,
+    required this.finishDate,
+    required this.finishTime,
+    required this.distance,
+    required this.timeSpent,
+    required this.type,
+    required this.routeId,
+    required this.userId,
+  });
+
+  factory RouteData.fromJson(Map<String, dynamic> json) {
+    return RouteData(
+      startDate: json['start_date'],
+      startTime: json['start_time'],
+      finishDate: json['finish_date'],
+      finishTime: json['finish_time'],
+      distance: json['distance'].toDouble(),
+      timeSpent: json['time_spent'],
+      type: json['type'],
+      routeId: json['route_id'],
+      userId: json['user_id'],
     );
   }
 }
