86a14e147a6e20007495bad2dfed1091f2e595a5
Zenemi44 <107159982+Zenemi44@users.noreply.github.com>
2023-03-20 14:18:58 -0500

    mapa agregado

--- lib/activity_screen.dart
+++ lib/activity_screen.dart
@@ -1,7 +1,23 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'widgets/activity_widget.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class ActivityScreen extends StatelessWidget {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+      target: LatLng(10.96854, -74.78132),
+      tilt: 59.440717697143555,
+      zoom: 15.151926040649414);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,11 +30,25 @@ class ActivityScreen extends StatelessWidget {
             color: Colors.black.withOpacity(0.2),
           ),
           Center(
-            child: Text('Map goes here'),
-          ),
+              child: GoogleMap(
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          )),
           ActivityWidget(),
         ],
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: const Text('To the lake!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
