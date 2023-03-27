afd4450f05e396e9fcb38cc6275c599a60a30bd1
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2023-03-26 22:46:47 -0500

    Se agregó el test de la ventana de dialogo de log out

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,55 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:exercise_tracker/main.dart';
+import '../lib/login_screen.dart';
+import '../lib/widgets/log_out_dialog_widget.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('LogOutDialogWidget displays correctly',
+      (WidgetTester tester) async {
+    try {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: ElevatedButton(
+              onPressed: () async {
+                await showDialog(
+                  context: tester
+                      .element(find.byType(ElevatedButton))
+                      .findAncestorStateOfType<ScaffoldState>()!
+                      .context,
+                  builder: (context) {
+                    return const LogOutDialogWidget();
+                  },
+                );
+              },
+              child: const Text('Show dialog'),
+            ),
+          ),
+        ),
+      );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Tap on the "Show dialog" button
+      await tester.tap(find.text('Show dialog'));
+      await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Check if the dialog title and content are displayed correctly
+      expect(find.text('Cerrar sesión'), findsOneWidget);
+      expect(
+          find.text('¿Estás seguro de querer cerrar sesión?'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Tap on the "Cancelar" button and check if the dialog is closed
+      await tester.tap(find.text('Cancelar'));
+      await tester.pumpAndSettle();
+      expect(find.text('Cerrar sesión'), findsNothing);
+
+      // Tap on the "Salir" button and check if the dialog is closed and the LoginScreen is displayed
+      await tester.tap(find.text('Salir'));
+      await tester.pumpAndSettle();
+      expect(find.byType(LoginScreen), findsOneWidget);
+    } catch (error) {
+      print(error);
+    }
   });
 }
